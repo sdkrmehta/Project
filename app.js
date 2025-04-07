8af71e2ea1e5560c142ab1d730cc973663473d6d
unknown <surajdeepkumarmehta@gmail.com>
2025-04-07 08:45:02 +0530

    Fixed dependency conflict with cloudinary

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const localStrategy = require("passport-local");
@@ -20,7 +21,8 @@ const listingsRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = "mongodb://localhost:27017/wonderlust";
+// const MONGO_URL = "mongodb://localhost:27017/wonderlust";
+const dbUrl = process.env.ATLASDB_URL;
 
 main()
 	.then(() => {
@@ -31,7 +33,7 @@ main()
 	});
 
 async function main() {
-	await mongoose.connect(MONGO_URL);
+	await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -42,7 +44,20 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "assets")));
 
+const store = MongoStore.create({
+	mongoUrl: dbUrl,
+	crypto: {
+		secret: process.env.SECRET,
+	},
+	touchAfter: 24 * 3600,
+});
+
+store.on("error", () => {
+	console.log("ERROR IN MONGO SESSION STORE", err);
+});
+
 const sessionOptions = {
+	store,
 	secret: process.env.SECRET,
 	resave: false,
 	saveUninitialized: true,
